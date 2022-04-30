179e7e98dc2e55ab76906c4e405b192845689891
Albert Llousas <albert.llousas@n26.com>
2022-04-30 11:35:57 +0200

    Fix foldr

--- src/MarsRover.hs
+++ src/MarsRover.hs
@@ -4,13 +4,13 @@ module MarsRover where
 import Control.Monad.Trans.Reader
 import Data.Functor
 
-newtype Grid = Grid (Int, Int) deriving (Eq, Show)
+data Grid = Grid Size [Obstacle] deriving (Eq, Show)
 
 data Direction = North | East | South | West deriving (Eq, Show, Enum)
 
-data Position = Position (Int, Int) Direction deriving (Eq, Show)
+data Position = Position Point Direction deriving (Eq, Show)
 
-data Rover = Rover { position :: Position, obstacle :: Maybe Position } deriving (Eq, Show)
+data Rover = Rover { position :: Position, obstacle :: Maybe Obstacle } deriving (Eq, Show)
 
 data Command =  MoveForward | MoveBackwards | TurnLeft | TurnRight deriving (Eq, Show)
 
@@ -18,28 +18,39 @@ data RoverError = InvalidCommand String deriving (Eq, Show)
 
 type ParseCommands = String -> Either RoverError [Command]
 
+type Obstacle = (Int, Int)
+
+type Point = (Int, Int)
+
+type Size = (Int, Int)
+
 data Dependencies =  Dependencies { parseCommands ::ParseCommands }
 
 run :: Rover -> Grid -> String -> Either RoverError Rover
 run rover grid commands = runReaderT (execute rover grid commands) Dependencies { parseCommands = defaultParseCommands }
 
 execute :: Rover -> Grid -> String -> ReaderT Dependencies (Either RoverError) Rover
-execute rover grid unparsedCommands = ReaderT (\deps -> let executeCommands commands = foldr (\c r -> executeCommand c r grid) rover commands
+execute rover grid unparsedCommands = ReaderT (\deps -> let executeCommands commands = foldl (\r c -> executeCommand c r grid) rover commands
                                                             commands = parseCommands deps $ unparsedCommands
                                                         in executeCommands <$> commands)
 
 executeCommand :: Command -> Rover -> Grid -> Rover
 executeCommand _ stuck@Rover { obstacle = Just _ } _ = stuck
-executeCommand TurnLeft rover@Rover { position = Position xy dir} grid = rover { position = Position xy (turnLeft dir) }
-executeCommand TurnRight rover@Rover { position = Position xy dir} grid = rover { position = Position xy (turnRight dir) }
-executeCommand MoveForward rover grid = moveOnXAxis rover grid 1
-executeCommand MoveBackwards rover grid = moveOnXAxis rover grid (-1)
+executeCommand TurnLeft rover@Rover { position = Position point dir} grid = rover { position = Position point (turnLeft dir) }
+executeCommand TurnRight rover@Rover { position = Position point dir} grid = rover { position = Position point (turnRight dir) }
+executeCommand MoveForward rover grid = case move (position rover) grid 1 of Left o -> rover { obstacle = Just o }; Right p -> rover { position = p }
+executeCommand MoveBackwards rover grid = case move (position rover) grid (-1) of Left o -> rover { obstacle = Just o }; Right p -> rover { position = p }
 
-moveOnXAxis :: Rover -> Grid -> Int -> Rover
-moveOnXAxis rover@Rover { position = Position (x,y) dir} grid positions = rover { position = Position (wrap (x, y + positions) grid) dir }
+move :: Position -> Grid -> Int -> Either Obstacle Position
+move (Position (x,y) North) (Grid size obstacles) n = checkObstacle (Position (wrap (x, y + n) size) North) obstacles
+move (Position (x,y) South) (Grid size obstacles) n = checkObstacle (Position (wrap (x, y - (n)) size) South) obstacles
+move (Position (x,y) East) (Grid size obstacles) n = checkObstacle (Position (wrap (x + n, y) size) East) obstacles
+move (Position (x,y) West) (Grid size obstacles) n = checkObstacle (Position (wrap (x - (n), y) size) West) obstacles
 
-wrap :: (Int, Int) -> Grid -> (Int, Int)
-wrap (x, y) (Grid (z, w)) = (x `mod` z,  y `mod` w)
+checkObstacle (Position point dir) obstacles = if(point `elem` obstacles) then Left point else Right (Position point dir)
+
+wrap :: Point -> Size -> Point
+wrap (x, y) (z, w) = (x `mod` z,  y `mod` w)
 
 turnLeft North = West
 turnLeft direction = pred direction
--- test/MarsRoverSpec.hs
+++ test/MarsRoverSpec.hs
@@ -3,60 +3,80 @@ module MarsRoverSpec where
 import Test.Hspec
 import MarsRover as Rover
 import Control.Monad.Trans.Reader
+import Data.List.Split
 
 spec :: Spec
 spec = do
 
   let initialRover = Rover { position = (Position (0,0) North), obstacle = Nothing }
-  let grid = Grid (5,5)
+  let grid = Grid (5,5) []
 
-  describe "Mars Rover" $ do
+  describe "Run commands " $ do
 
-    describe "Run commands " $ do
+    it "should turn left" $ do
+      let rover = Rover.run initialRover grid "l"
+      rover `shouldBe` Right (Rover { position = (Position (0,0) West), obstacle = Nothing })
 
-      it "should turn left" $ do
-        let rover = Rover.run initialRover grid "l"
-        rover `shouldBe` Right (Rover { position = (Position (0,0) West), obstacle = Nothing })
+    it "should turn right" $ do
+      let rover = Rover.run initialRover grid "r"
+      rover `shouldBe` Right (Rover { position = (Position (0,0) East), obstacle = Nothing })
 
-      it "should turn right" $ do
-        let rover = Rover.run initialRover grid "r"
-        rover `shouldBe` Right (Rover { position = (Position (0,0) East), obstacle = Nothing })
+    it "should move forward" $ do
+      let rover = Rover.run initialRover grid "f"
+      rover `shouldBe` Right (Rover { position = (Position (0,1) North), obstacle = Nothing })
 
-      it "should move forward" $ do
-        let rover = Rover.run initialRover grid "f"
-        rover `shouldBe` Right (Rover { position = (Position (0,1) North), obstacle = Nothing })
+    it "should move backwards" $ do
+      let rover = Rover.run Rover { position = (Position (4,4) North), obstacle = Nothing } grid "b"
+      rover `shouldBe` Right (Rover { position = (Position (4,3) North), obstacle = Nothing })
 
-      it "should move backwards" $ do
-        let rover = Rover.run Rover { position = (Position (4,4) North), obstacle = Nothing } grid "b"
-        rover `shouldBe` Right (Rover { position = (Position (4,3) North), obstacle = Nothing })
+    it "should run a sequence of commands" $ do
+      let rover = Rover.run initialRover grid "rflffb"
+      rover `shouldBe` Right (Rover { position = (Position (1,1) North), obstacle = Nothing })
 
-    describe "Grid wrap" $ do
+  describe "Grid wrap" $ do
 
-      it "should move from one edge of the grid to another when the rover goes over the boundaries of the grid" $ do
-        let roverInTheEdge = Rover { position = (Position (1,4) North), obstacle = Nothing }
-        let rover = Rover.run roverInTheEdge grid "f"
-        rover `shouldBe` Right (Rover { position = (Position (1,0) North), obstacle = Nothing })
+    it "should move from one edge of the grid to another when the rover goes over the boundaries of the grid" $ do
+      let roverInTheEdge = Rover { position = (Position (1,4) North), obstacle = Nothing }
+      let rover = Rover.run roverInTheEdge grid "f"
+      rover `shouldBe` Right (Rover { position = (Position (1,0) North), obstacle = Nothing })
 
-    describe "Default command parsing" $ do
+  describe "Obstacle detection" $ do
 
-      it "should parse 'l' as turn left command" $ do
-        defaultParseCommands "l" `shouldBe` Right [TurnLeft]
+    it "should move up to the last possible point when there is an obstacle in the way and report it" $ do
+      let gridWithObstacles = Grid (5,5) [(0,4)]
+      let rover = Rover.run initialRover gridWithObstacles "fffffff"
+      rover `shouldBe` Right (Rover { position = (Position (0,3) North), obstacle = Just (0, 4) })
+  --
+  --
+  --    // rest of test, dir, wrap, run, exec
 
-      it "should parse 'r' as turn right command" $ do
-        defaultParseCommands "r" `shouldBe` Right [TurnRight]
+  describe "Default command parsing" $ do
 
-      it "should parse 'f' as move forward command" $ do
-        defaultParseCommands "f" `shouldBe` Right [MoveForward]
+    it "should parse 'l' as turn left command" $ do
+      defaultParseCommands "l" `shouldBe` Right [TurnLeft]
 
-      it "should parse 'b' as move backward command" $ do
-        defaultParseCommands "b" `shouldBe` Right [MoveBackwards]
+    it "should parse 'r' as turn right command" $ do
+      defaultParseCommands "r" `shouldBe` Right [TurnRight]
 
-      it "should fail parsing an invalid string as a command" $ do
-        defaultParseCommands "x" `shouldBe` Left (InvalidCommand "x")
+    it "should parse 'f' as move forward command" $ do
+      defaultParseCommands "f" `shouldBe` Right [MoveForward]
 
---    describe "Execute commands" $ do
---
---      it "should execute a command to turn right" $ do
---        let rover = Rover.execute initialRover grid "r"
---        let result = runReaderT rover Dependencies { parseCommands = (\_ -> Right [TurnRight]) }
---        result `shouldBe` Right (Rover { position = (Position (0,0) East), obstacle = Nothing })
+    it "should parse 'b' as move backward command" $ do
+      defaultParseCommands "b" `shouldBe` Right [MoveBackwards]
+
+    it "should fail parsing an invalid string as a command" $ do
+      defaultParseCommands "x" `shouldBe` Left (InvalidCommand "x")
+
+  describe "Execute commands providing a different format" $ do
+
+    let differentParseCommands unparsedCommands = sequenceA (parseCommand <$>  (splitOn "#" unparsedCommands))
+          where parseCommand "FORWARD" = Right MoveForward
+                parseCommand "BACKWARDS" = Right MoveBackwards
+                parseCommand "LEFT" = Right TurnLeft
+                parseCommand "RIGHT" = Right TurnRight
+                parseCommand invalid = Left $ InvalidCommand invalid
+
+    it "should execute commands" $ do
+      let rover = Rover.execute initialRover grid "RIGHT#FORWARD#LEFT#FORWARD#FORWARD#BACKWARDS"
+      let result = runReaderT rover Dependencies { parseCommands = differentParseCommands }
+      result `shouldBe` Right (Rover { position = (Position (1,1) North), obstacle = Nothing })
